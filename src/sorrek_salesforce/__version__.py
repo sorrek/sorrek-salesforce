0f66b710c1d9eb921da8323c6e86ee2dd4560929
Ariel Lavi <ariel@sorrek.io>
2024-01-11 04:19:33 -0500

    Versioning updates

--- src/sorrek_salesforce/__version__.py
+++ src/sorrek_salesforce/__version__.py
@@ -2,10 +2,12 @@
 
 __title__ = 'sorrek-salesforce'
 __url__ = 'https://github.com/sorrek/sorrek-salesforce'
-__version__ = '1.0.0'
+__home_page__ = 'https://github.com/sorrek/sorrek-salesforce'
+__version__ = '1.0.1'
 __author__ = 'Ariel Lavi'
 __author_email__ = 'ariel@sorrek.io'
 __license__ = 'MIT'
 __maintainer__ = 'Ariel Lavi'
 __maintainer_email__ = 'ariel@sorrek.io'
 __keywords__ = 'python sql salesforce salesforce.com sorrek sorrek.io'
+__requires__ = 'numpy, pandas'
